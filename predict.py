7b7d7a6de267b5b4a581a8a2f9a5e6bf9d65f3a0
John Howe <94157812+johnh-otu@users.noreply.github.com>
2025-03-22 16:33:39 -0400

    feat: transaction and portfolio simulation

--- predict.py
+++ predict.py
@@ -1,4 +1,6 @@
 # Import Libraries
+import os
+import json
 import yfinance as yf
 import pandas as pd
 import numpy as np
@@ -15,8 +17,13 @@ threshold = 0.005  # threshold for decision making
 capital = 10000  # starting capital in USD
 transaction_fee_rate = 0.01  # 1% fee
 features = ['Open', 'High', 'Low', 'Close', 'Volume', 'Rolling_Mean', 'Spread']
+portfolio_filename = "portfolio.json"
+current_price = 999999999999999.99
+model_path = "" # TODO: Add model path
+sell_ratio = 0.4 # sell 40% of shares (originally 10%)
+buy_ratio = 0.1 # buy with 10% of capital
 
-# Load model
+# TODO: Load model
 def get_model(path):
     # use libjar/pickle to load model
     print(f"loaded model from {path}")
@@ -27,7 +34,7 @@ def get_data():
     if data.empty:
         raise ValueError("No data retrived. Check network connection.")
     
-    last_20_adj_close = data["Adj Close"].tail(20)
+    last_20_adj_close = data["Adj Close"].tail(window)
     rolling_mean = last_20_adj_close.rolling(window=window).mean().iloc[-1]
 
     yesterday = data.iloc[-1]
@@ -35,8 +42,8 @@ def get_data():
     yesterday["Spread"] = (yesterday["Adj Close"] - rolling_mean) / rolling_mean
     return yesterday[features]
 
-# Run model evaluation
-def evaluate(model, data):
+# Run model prediction
+def predict(model, data):
     latest_features = data[features].values.reshape(1, -1)
     predicted_return = model.predict(latest_features)[0]
     spread = data['Spread']
@@ -65,33 +72,87 @@ def make_decision(predicted_return, spread):
 
     return decision
 
+def get_portfolio_data():
+    
+    default = {"shares": 0, "capital": capital}
+    portfolio_data = {}
+
+    # Check if portfolio file exists
+    #   - If exists -> import data
+    #   - If not exists -> generate new file and use default data
+    if not os.path.exists(portfolio_filename):
+        return update_portfolio(default)
+    else:
+        try:
+            with open(portfolio_filename, "r") as fp:
+                portfolio_data = json.load(fp)
+        except json.JSONDecodeError:
+            # if corrupt or empty, reset to default
+            portfolio_data = default
+            update_portfolio(portfolio_data)
+        
+        # check all expected keys exist
+        for key, val in default.items():
+            if key not in portfolio_data:
+                portfolio_data[key] = val
+    
+    return portfolio_data
+
+def update_portfolio(portfolio):
+    with open(portfolio_filename, "w") as fp:
+            json.dump(portfolio, fp, indent=4)
+    return portfolio
+
 # Simulate order details based on the decision.
-def simulate_order(decision):
-    # TODO: Implement a more robust way of tracking which orders have been made (i.e. csv or external file)
-    # Number of shares held should be based on actual value
+def simulate_order(decision, current_price):
+
+    portfolio = get_portfolio_data()
 
     if decision == "Buy":
         # Example: invest 10% of current capital (before transaction fees)
-        buy_amount = capital * 0.1
+        buy_amount = capital * buy_ratio
         # Adjust for a 1% fee
         buy_amount_after_fee = buy_amount * (1 - transaction_fee_rate)
-        print(f"Buy: ${buy_amount_after_fee:.2f} worth of shares.")
-    elif decision == "Sell":
-        # Example: assume you hold some shares; here, we'll simulate selling 10% of a placeholder holding.
-        num_shares_held = 10  # This should come from your portfolio tracking system.
-        shares_to_sell = max(int(num_shares_held * 0.1), 1)
-        print(f"Sell: {shares_to_sell} shares (after applying transaction fees to proceeds).")
-    else:
-        print("Hold: No transaction will be made.")
+        shares_to_buy = int(buy_amount / current_price)
 
-    return 0
+        if shares_to_buy == 0:
+            return "Hold: No transaction will be made."
+        else:
+            buy_amount = shares_to_buy * current_price
+            buy_amount_after_fee = buy_amount * (1 - transaction_fee_rate)
+            portfolio["capital"] -= buy_amount_after_fee
+            portfolio["shares"] += shares_to_buy
+            update_portfolio(portfolio)
+            return f"Buy: ${buy_amount_after_fee:.2f} worth of shares."
+    elif decision == "Sell":
+        # Simulate selling 10% of a placeholder holding.
+        num_shares_held = portfolio["shares"]
+
+        if (num_shares_held < 1):
+            return "Hold: No transaction will be made."
+
+        shares_to_sell = max(int(num_shares_held * sell_ratio), 1)
+        return f"Sell: {shares_to_sell} shares (after applying transaction fees to proceeds)."
+    else:
+        return "Hold: No transaction will be made."
+
+def get_current_price():
+    tckr = yf.Ticker(ticker)
+    price = tckr.info.get("regularMarketPrice")
+    if price:
+        return price
+    else:
+        return current_price
 
 def main():
-    model_path = ""
-
     # Use model with recent data to predict today's return and spread
-    pred_return, spread = evaluate(get_model(model_path), get_data())
+    pred_return, spread = predict(get_model(model_path), get_data())
     # Make decision (Buy, Sell, Hold)
     decision = make_decision(pred_return, spread)
-    simulate_order(decision)
+
+    # Get current price for buy/sell orders
+    if decision != "Hold":
+        current_price = get_current_price()
+
+    print(simulate_order(decision, current_price))
     return 0
